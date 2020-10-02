aae67f856ffb7d5b60a15279dd498e104bb4aac5
supermax500 <71511189+supermax500@users.noreply.github.com>
2020-10-02 22:51:06 +0500

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -2,21 +2,29 @@
 #include <math.h>
 #include <cstdlib>
 #include <QDebug>
+#include <QRandomGenerator>
+#define qrand() QRandomGenerator::system()->generate()
+
 int Game::color1;
 int Game::color2;
 int Game::c1;
 int Game::c2;
-const QColor Game::col[16]={
-    QColor(0xf0ff0000), QColor(0xf000ff00), QColor(0xf00000ff), QColor(0xf0ffffff), //Red Grn Blu Wht
-    QColor(0xf0ffff00), QColor(0xf0000000), QColor(0xf000ffff), QColor(0xf0ff00ff), //Yel Blk Lbl Pnk
-    QColor(0xf000ff7f), QColor(0xf0007fff), QColor(0xf0007f7f), QColor(0xf07f7f7f), //USD Sky Wav Gry
-    QColor(0xf07f0000), QColor(0xf0ff7f00), QColor(0xf000007f), QColor(0xf07f007f) //Bld Orn Dbl Prp
+QColor Game::buttoncolor;
+//color array for button and background
+const QColor Game::col[16]=
+{
+    QColor(0xff4f0014), QColor(0xff00ff00), QColor(0xff122faa), QColor(0xffffffff), //Red Lim Blu Wht
+    QColor(0xffffff00), QColor(0xff000000), QColor(0xfffcdd76), QColor(0xffff1493), //Yel Blk Snd Pnk
+    QColor(0xff442d25), QColor(0xffffb28b), QColor(0xff4b0082), QColor(0xff876c99), //Cof Sky Ind Gry
+    QColor(0xff008000), QColor(0xffff4f00), QColor(0xff002f55), QColor(0xffc10020)  //Gre Orn Dbl Prp
 };
-const QString Game::cs[16]={
-    "Red", "Green", "Blue", "White",
-    "Yellow", "Black", "Light blue", "Pink",
-    "Light green", "Sky", "Wave", "Gray",
-    "Blood", "Orange", "Dark blue", "Purple"
+//corresponding color names
+const QString Game::cs[16]=
+{
+    "Deep red", "Lime", "BSOD", "White",
+    "Yellow", "Black", "Sand", "Pink",
+    "Coffee", "Yellow-pink", "Indigo", "Light Purp",
+    "Green", "Orange", "Deep blue", "Red"
 };
 
 Game::Game()
@@ -29,8 +37,9 @@ Game::Game()
 
 bool Game::check()
 {
-    //qDebug() << "color1  = " << color1 << "color2 = " << color2 << "c1 = "<<  c1 << "c2 = " << c2 << "tick = " << tick;
-    if((color1 == c1 && color2 == c2) || (color1 == c2 && color2 == c1)){
+    //check if colors match after press
+    if((color1 == c1 && color2 == c2) || (color1 == c2 && color2 == c1))
+    {
         generate();
         return 1;
     }
@@ -41,24 +50,26 @@ void Game::update(widget &W)
 {
     currentTick++;
     //current colors
-    if(currentTick >= tick){
+    if(currentTick >= tick)
+    {
         int t = qrand() % 2;
         c1 = color1 * t + color2 * (1 - t);
         c2 = color2 * t + color1 * (1 - t);
         tick = qrand() % 7 + 1;
         currentTick = 0;
     }
-    else{//16 - nomb of colors
+    else //16 - nomb of colors
+    {
         c1 = qrand() % 16;
         c2 = qrand() % 16;
     }
-    //W.RoundButton.setColor(c1);
     W.setColor(c2);
 }
 
 void Game::generate()
 {
-    tick = qrand() % 7+ 1;// через сколько апдейтов будет гарантировано правильная комбинация
+    tick = qrand() % 7+ 1;// pseudorandom, necessary color combimation guaranteed to appear
+    //generate random colors
     color1 = qrand() % 16;
     color2 = qrand() % 16;
 }
